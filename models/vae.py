2576ce20168e732c6bb30ef3e788735e8e607bb9
Ivy Zhang <zivy042003@gmail.com>
2024-12-06 12:28:50 -0500

    removed biases before batchnorm

--- models/vae.py
+++ models/vae.py
@@ -9,58 +9,71 @@ class VAE(nn.Module):
         self.encoder = VariationalEncoder(latent_dims, in_c)
         self.decoder = Decoder(latent_dims, in_c)
 
+        self.N = torch.distributions.Normal(0, 1)
+        self.kl = 0
+
     def forward(self, x):
-        z = self.encoder(x)
+        mu, logvar = self.encoder(x)
+        self.kl = torch.mean(
+            -0.5 * torch.sum(1.0 + logvar - mu**2 - logvar.exp(), dim=1), dim=0
+        )
+
+        z = self.reparamterize(mu, logvar)
         return self.decoder(z)
+
+    def reparamterize(self, mu, logvar):
+        sigma = torch.exp(logvar / 2.0)
+        eps = self.N.sample(mu.shape)
+        return mu + sigma * eps
+    
+    def encode(self, x):
+        return self.encoder(x)
 
 
 class VariationalEncoder(nn.Module):
     def __init__(self, latent_dims, in_c):
         super().__init__()
         self.conv_layers = nn.Sequential(
-            nn.Conv2d(in_c, 32, 5, 2),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, 5, 2),
-            nn.ReLU(),
-            nn.Conv2d(64, 128, 5, 2),
-            nn.ReLU(),
-            nn.Conv2d(128, 256, 5, 2),
-            nn.ReLU(),
+            nn.Conv2d(in_c, 32, 5, 2, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
+            nn.Conv2d(32, 64, 5, 2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.Conv2d(64, 128, 5, 2, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.Conv2d(128, 256, 5, 2, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(),
         )
 
         # bx96x96x3 -> bx3x3x128
-        self.mu_lin = nn.Linear(3*3*256, latent_dims)
-        self.sigma_lin = nn.Linear(3*3*256, latent_dims)
-
-        self.N = torch.distributions.Normal(0, 1)
-        self.kl = 0
+        self.mu_lin = nn.Linear(3 * 3 * 256, latent_dims)
+        self.sigma_lin = nn.Linear(3 * 3 * 256, latent_dims)
 
     def forward(self, x):
-        # print(f'shape before conv: {x.shape}')
         x = self.conv_layers(x)
-        # print(f'shape after conv: {x.shape}')
         x = torch.flatten(x, start_dim=1)
-        # print(f'shape after flatten: {x.shape}')
         mu = self.mu_lin(x)
-        sigma = torch.exp(self.sigma_lin(x) / 2.0)
-        z = mu + sigma * self.N.sample(mu.shape)
-        self.kl = (sigma**2 + mu**2 - torch.log(sigma) - 0.5).sum()
-
-        # print(f'after: {z.shape}')
-        return z
+        logvar = self.sigma_lin(x)
+        return [mu, logvar]
 
 
 class Decoder(nn.Module):
     def __init__(self, latent_dims, in_c):
         super().__init__()
-        self.linear = nn.Linear(latent_dims, 3*3*256)
+        self.linear = nn.Linear(latent_dims, 3 * 3 * 256)
         self.conv_layers = nn.Sequential(
-            nn.ConvTranspose2d(256, 128, 5, 2), 
-            nn.ReLU(),
-            nn.ConvTranspose2d(128, 64, 5, 2),
-            nn.ReLU(),
-            nn.ConvTranspose2d(64, 32, 6, 2),
-            nn.ReLU(),
+            nn.ConvTranspose2d(256, 128, 5, 2, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(128, 64, 5, 2, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(),
+            nn.ConvTranspose2d(64, 32, 6, 2, bias=False),
+            nn.BatchNorm2d(32),
+            nn.LeakyReLU(),
             nn.ConvTranspose2d(32, in_c, 6, 2),
             nn.Sigmoid(),
         )
