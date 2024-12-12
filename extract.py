1164b6b7946ad451c5ea000d7dc3fd5b0d82b8bc
Ivy Zhang <zivy042003@gmail.com>
2024-12-12 13:09:49 -0500

    extract with controller

--- extract.py
+++ extract.py
@@ -5,6 +5,12 @@ import gymnasium as gym
 import numpy as np
 import os
 import random
+import torch
+torch.set_grad_enabled(False)
+
+from models.rnn import MDNRNN
+from models.vae import VAE
+from models.controller import Controller
 
 # Constants
 # TODO: Turn this into command line prompts
@@ -14,16 +20,56 @@ DIR_NAME = "rollouts"
 NUM_WORKERS = min(64, cpu_count())
 
 MAX_FRAMES = 1_000
-TOTAL_ROLLOUTS = 500
+TOTAL_ROLLOUTS = 200
 TRIALS_PER_WORKER = TOTAL_ROLLOUTS // NUM_WORKERS + 1
+
+USE_AGENT = True
+
+#### USE AGENT PARAMETERS ####
+
+VAE_CHECKPOINTS = 'vae_checkpoints'
+RNN_CHECKPOINTS = 'rnn_checkpoints'
+AGENT_CHECKPOINTS = 'agent_checkpoints'
+
+VAE_CHECKPOINT_N = '9'
+RNN_CHECKPOINT_N = '450'
+
+# TODO: load the VAE and RNN and env
+VAE_LATENT_DIMS = 32 
+
+
+RNN_INPUT_SIZE = 32 + 3 # latent_dims + action_size
+RNN_HIDDEN_SIZE = 128
+RNN_OUTPUT_SIZE = 32
+N_GAUSSIANS = 5
+N_LAYERS = 1
+
+
+POP_SIZE = 32
+ACT_INPUT_SIZE = RNN_HIDDEN_SIZE + VAE_LATENT_DIMS
+ACT_SPACE = 3
+
+####
 
 if not os.path.exists(DIR_NAME):
     os.mkdir(DIR_NAME)
 
 
 def generate_rollouts(worker_id):
+    if USE_AGENT:
+        vae = VAE(latent_dims=32, in_c=3)
+        vae.load_state_dict(torch.load(os.path.join(VAE_CHECKPOINTS, f'{VAE_CHECKPOINT_N}.pth'), weights_only=True))
+        vae.eval()
+
+        rnn = MDNRNN(N_GAUSSIANS, RNN_INPUT_SIZE, RNN_HIDDEN_SIZE, RNN_OUTPUT_SIZE, N_LAYERS)
+        rnn.load_state_dict(torch.load(os.path.join(RNN_CHECKPOINTS, f'{RNN_CHECKPOINT_N}.pth'), weights_only=True))
+        rnn.eval()
+
+        c_data = np.load(os.path.join(AGENT_CHECKPOINTS, 'best_controller.npy'))
+        controller = Controller(c_data, ACT_INPUT_SIZE, ACT_SPACE)
+
     env = gym.make(
-        "CarRacing-v2",
+        "CarRacing-v3",
         render_mode="rgb_array",
         lap_complete_percent=0.95,
         domain_randomize=False,
@@ -37,11 +83,18 @@ def generate_rollouts(worker_id):
         filename = f'{DIR_NAME}/{worker_id}_{seed}.npz'
         obs_l = []
         act_l = []
+        h = rnn.initial_state(1)
 
         # NOTE: obs is 96x96
         obs, info = env.reset(seed=seed)
         for frame in range(MAX_FRAMES):
-            act = env.action_space.sample()
+            if not USE_AGENT:
+                act = env.action_space.sample()
+            else:
+                mu, logvar = vae.encode(torch.tensor(obs, dtype=torch.float32).permute(2, 0, 1).unsqueeze(0))
+                z = vae.reparamterize(mu, logvar)
+                c_in = torch.cat([z.flatten(), h[1].flatten()], dim=-1).numpy()
+                act = controller.forward(c_in)
 
             obs_l.append(obs)
             act_l.append(act)
@@ -49,6 +102,9 @@ def generate_rollouts(worker_id):
             obs, reward, terminated, truncated, info = env.step(act)
             if terminated or truncated:
                 break
+
+            if USE_AGENT:
+                _, _, _, h = rnn(torch.cat([z.flatten(), torch.tensor(act, dtype=torch.float32)], dim=-1).unsqueeze(0).unsqueeze(0), h)
 
         TOTAL_FRAMES += frame + 1
         print(f'Worker {worker_id} [{trial+1}/{TRIALS_PER_WORKER}] ({TOTAL_FRAMES} frames)')
